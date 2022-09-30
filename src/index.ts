cf23fc4cf8e99e94f545a03cbb4faf9d9f4059ab
MarkSFrancis <mrkfrancis8@gmail.com>
2022-09-30 16:42:05 +0100

    Created initial layout and sample command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import { program } from 'commander'
 import { sum } from './math'
 
-console.info(sum(1, 2))
+program
+  .command('add')
+  .description('add numbers together')
+  .argument('<numbers...>', 'numbers to add')
+  .action((numbers) => {
+    console.log({ numbers })
+    console.log(`Sum: ${sum(...numbers.map(Number))}`)
+  })
+
+program.parse()
