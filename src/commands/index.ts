adf7ea38dfaa2a73172b14aa58f497b921d8c179
MarkSFrancis <mrkfrancis8@gmail.com>
2022-10-10 11:18:43 +0100

    Remove unused sum command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,8 @@
 import { program } from 'commander'
 import { docsCommand } from './docs'
-import { sumCommand } from './sum'
 import { tvCommand } from './tv'
 
 program.addCommand(docsCommand)
-program.addCommand(sumCommand)
 program.addCommand(tvCommand)
 
 program.parse()
