6f431015e55b00aba8e09d5b3e5bd4a6cc47828a
MarkSFrancis <mrkfrancis8@gmail.com>
2022-10-12 19:33:41 +0100

    Fixed bad commands index

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,10 @@
 import { consolePrompt } from '@/lib/console/askUserForInput'
 import { program } from 'commander'
-import { crawlCommand } from './crawl'
 import { docsCommand } from './docs'
 import { tvCommand } from './tv'
 
 program.addCommand(docsCommand)
 program.addCommand(tvCommand)
-program.addCommand(crawlCommand)
 
 program.action(async () => {
   const response = await consolePrompt('What is your name?')
