d4c4b3573d0b2b64b2fcc5ac3eedeb0b55c424b4
MarkSFrancis <mrkfrancis8@gmail.com>
2022-10-29 23:28:00 +0100

    Added tests for language detection

---
+++ src/lib/language/findLanguageByCode.test.ts
@@ -0,0 +1,31 @@
+import { findLanguageByCode } from './findLanguageByCode'
+
+it('should return English for "English"', () => {
+  const language = findLanguageByCode('English')
+
+  expect(language?.name).toEqual('English')
+})
+
+it('should return `undefined` for ""', () => {
+  const language = findLanguageByCode('')
+
+  expect(language).toBeUndefined()
+})
+
+it('should return English for "ENGLISH"', () => {
+  const language = findLanguageByCode('ENGLISH')
+
+  expect(language?.name).toEqual('English')
+})
+
+it('should return Modern Greek (1453-) for "Greek"', () => {
+  const language = findLanguageByCode('Greek')
+
+  expect(language?.name).toEqual('Modern Greek (1453-)')
+})
+
+it('should return Norwegian Bokmål for "Bokmal"', () => {
+  const language = findLanguageByCode('Bokmal')
+
+  expect(language?.name).toEqual('Norwegian Bokmål')
+})
