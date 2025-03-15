7f90f9faa5f503af823e4734fc054c17f69a599f
Mark Francis <mark@moonstar.ai>
2025-03-15 21:47:37 +0000

    Updated special folders to support MacOS

--- src/lib/paths/SpeciaFolders.ts
+++ src/lib/paths/SpeciaFolders.ts
@@ -1,9 +1,44 @@
-export enum SpecialFolders {
-  Books = '/mnt/z/media/books/',
-  Clips = '/mnt/z/media/clips/',
-  Downloads = '/mnt/z/active-downloads/',
-  Movies = '/mnt/z/media/movies/',
-  Music = '/mnt/z/media/music/library/',
-  Pictures = '/mnt/z/media/pictures/',
-  Tv = '/mnt/z/media/tv-series',
+const osValue = <TValues extends string>(
+  args: Partial<Record<NodeJS.Platform, TValues>> & { default: TValues }
+): TValues => {
+  if (process.platform in args) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return args[process.platform]!
+  } else {
+    return args.default
+  }
 }
+
+export const SpecialFolders = {
+  Books: osValue({
+    darwin: '/Volumes/tank/media/books/',
+    default: '/mnt/z/media/books/',
+  }),
+  Clips: osValue({
+    darwin: '/Volumes/tank/media/clips/',
+    default: '/mnt/z/media/clips/',
+  }),
+  Downloads: osValue({
+    darwin: '/Volumes/tank/active-downloads/',
+    default: '/mnt/z/active-downloads/',
+  }),
+  Movies: osValue({
+    darwin: '/Volumes/tank/media/movies/',
+    default: '/mnt/z/media/movies/',
+  }),
+  Music: osValue({
+    darwin: '/Volumes/tank/media/music/library/',
+    default: '/mnt/z/media/music/library/',
+  }),
+  Pictures: osValue({
+    darwin: '/Volumes/tank/media/pictures/',
+    default: '/mnt/z/media/pictures/',
+  }),
+  Tv: osValue({
+    darwin: '/Volumes/tank/media/tv-series',
+    default: '/mnt/z/media/tv-series',
+  }),
+}
+
+export type SpecialFolders =
+  (typeof SpecialFolders)[keyof typeof SpecialFolders]
