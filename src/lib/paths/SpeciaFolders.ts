4b5382c7f94059efb3c931dfce733f65031288b0
Mark Francis <mark@moonstar.ai>
2025-05-12 20:19:42 +0100

    Updated special folders to add support for darwin

--- src/lib/paths/SpeciaFolders.ts
+++ src/lib/paths/SpeciaFolders.ts
@@ -1,9 +1,30 @@
-export enum SpecialFolders {
-  Books = '/mnt/z/media/books/',
-  Clips = '/mnt/z/media/clips/',
-  Downloads = '/mnt/z/active-downloads/',
-  Movies = '/mnt/z/media/movies/',
-  Music = '/mnt/z/media/music/library/',
-  Pictures = '/mnt/z/media/pictures/',
-  Tv = '/mnt/z/media/tv-series',
+import { platform } from 'os'
+
+const pathToShare = osValue({
+  default: '/mnt/z',
+  darwin: '/Volumes/tank',
+})
+
+export const SpecialFolders = {
+  Books: `${pathToShare}/media/books/`,
+  Clips: `${pathToShare}/media/clips/`,
+  Downloads: `${pathToShare}/active-downloads/`,
+  Movies: `${pathToShare}/media/movies/`,
+  Music: `${pathToShare}/media/music/library/`,
+  Pictures: `${pathToShare}/media/pictures/`,
+  Tv: `${pathToShare}/media/tv-series`,
+}
+
+export type SpecialFolders =
+  (typeof SpecialFolders)[keyof typeof SpecialFolders]
+
+function osValue<T>(
+  value: Partial<Record<NodeJS.Platform, T>> & { default: T }
+) {
+  const os = platform()
+  if (os in value) {
+    return value[os]
+  } else {
+    return value.default
+  }
 }
